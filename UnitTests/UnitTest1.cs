5fce000c48f5a62d4f4538e51ba83b5f7c8c3ef5
Max <MaxZiCode@gmail.com>
2026-09-25 18:14:50 +0300

    [RAP-1.1] Setup GitHub Actions CI Pipeline for .NET Aspire Solution (#9)

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,11 +1,10 @@
-﻿namespace UnitTests
+﻿namespace UnitTests;
+
+public class UnitTest1
 {
-    public class UnitTest1
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Test1()
-        {
-            Assert.True(false);
-        }
+        Assert.True(true);
     }
 }
